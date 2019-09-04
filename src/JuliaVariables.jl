fcb6193947cdc9b584c960a27d239f2b6369bb57
thautwarm <twshere@outlook.com>
2019-09-04 14:44:46 +0800

    WIP: distinguish let-scope from function-scope

--- src/JuliaVariables.jl
+++ src/JuliaVariables.jl
@@ -10,6 +10,9 @@ struct ScopedVal
     sym   :: Symbol
 end
 
+Base.show(io::IO, scopedval::ScopedVal) =
+    Base.print(io, string(objectid(scopedval.scope), base=60), "@", scopedval.sym)
+
 islinenumbernode(x) = x isa LineNumberNode
 rmlines(ex::Expr) = begin
     hd = ex.head
@@ -20,10 +23,12 @@ rmlines(a) = a
 
 function quick_lambda(ex)
     @when Expr(:call, args...) && if any(==(:_), args) end = ex begin
-        @gensym quick_arg
-        ex = Expr(:call, Any[arg === :_ ? quick_arg : quick_lambda(arg)
-                             for arg in args]...)
-        :($LegibleLambdas.@λ($quick_arg -> $ex))
+        quick_arg = gensym("quick_arg")
+        λ = gensym("λ")
+        args = Any[arg === :_ ? quick_arg : quick_lambda(arg)
+                   for arg in args]
+        ex = Expr(:call, args...)
+        :(function $λ($quick_arg) $ex end)
     @when Expr(hd, args...) = ex
         Expr(hd, map(quick_lambda, args)...)
     @otherwise
@@ -35,129 +40,140 @@ macro quick_lambda(ex)
     esc(quick_lambda(ex))
 end
 
-function func_name(ex)
+function with_fname(cont, ex)
     @match ex begin
-        Expr(:call, f, _...)  => f
-        :($a where {$(_...)}) => func_name(a)
-        :($a :: $_)           => func_name(a)
-        _                     => nothing
+        Expr(:call, f, args...)  => Expr(:call, cont(f), args...)
+        :($a where {$(args...)}) => :($(with_fname(cont, a)) where {$(args...)})
+        :($a :: $t)              => :($(with_fname(cont, a)) :: $t)
+        a                        => a
     end
 end
 
-@active FunSym(sym) begin
-    sym === :function
-end
+is_func_sym(sym) = sym === :function
 
-@active BroadcastFusing(sym) begin
+is_broadcast_fusing(sym) = begin
     s = string(sym)
     endswith(s, "=") && startswith(s, ".")
 end
 
-struct CtxFlag
-    is_local :: Bool
-    is_lhs   :: Bool
+is_rhs_sym(sym) = begin
+    (sym in Symbol[:ref, :.])
 end
 
-MLStyle.Record.@as_record
+@data ScopeDesc begin
+    Force(is_local :: Bool)
+    Lexical()
+end
 
-Base.:+(flag :: CtxFlag, desc :: Symbol) =
-    @when CtxFlag(is_local=is_local, is_lhs=is_lhs) = flag begin
+struct CtxFlag
+    default_scope :: ScopeDesc
+    is_lhs        :: Bool
+end
+MLStyle.Record.@as_record CtxFlag
+
+Base.@pure Base.:+(flag :: CtxFlag, desc :: Symbol) =
+    @when CtxFlag(default_scope=default_scope, is_lhs=is_lhs) = flag begin
         @match desc begin
-            :global => CtxFlag(false, is_lhs)
-            :local  => CtxFlag(true, is_lhs)
-            :lhs    => CtxFlag(is_local, true)
-            :rhs    => CtxFlag(is_local, false)
+            :lexical => CtxFlag(Lexical(), is_lhs)
+            :global  => CtxFlag(Force(false), is_lhs)
+            :local   => CtxFlag(Force(true), is_lhs)
+            :lhs     => CtxFlag(default_scope, true)
+            :rhs     => CtxFlag(default_scope, false)
         end
     @otherwise
         error("impossible")
     end
 
-CtxFlag() = CtxFlag(true, false)
+CtxFlag() = CtxFlag(Lexical(), false)
+
+function solve(ana, sym :: Symbol, ctx_flag::CtxFlag)
+    @when Force(is_local) = ctx_flag begin
+        if is_local
+            is_local!(ana, sym)
+        else
+            is_global!(ana, sym)
+        end
+    end
+    if ctx_flag.is_lhs
+        enter!(ana, sym)
+    else
+        require!(ana, sym)
+    end
+    ScopedVal(ana.solved.x, sym)
+end
 
 function solve(ana, ex, ctx_flag::CtxFlag = CtxFlag())
     @match ex begin
-        :($a where {$(tps...)}) =>
-            @quick_lambda begin
-                if !isempty(tps)
-                    ana = child_analyzer!(ana)
-                end
-                tps = map(solve(ana, _, ctx_flag + :lhs), tps)
-                a = solve(ana, a, ctx_flag)
-                :($a where {$(tps...)})
-            end
-        Expr(hd && FunSym(is_fn) && (:function || :(=) || :->), a,  b) =>
+        Expr(hd && if hd in (:function, :(=), :->) end, a,  b) =>
             begin
-                name = func_name(a)
-                if name !== nothing
-                    enter!(ana, name)
+                is_fn = is_func_sym(hd)
+                a = with_fname(a) do name
+                    is_fn = true
+                    !(name isa Symbol) && return name
+                    solve(ana, name, ctx_flag + :lhs)
                 end
-                new_ana = if is_fn || name !== nothing
+                ana = if is_fn
                     child_analyzer!(ana)
-                    ctx_flag = CtxFlag()
                 else
                     ana
                 end
-                a = solve(new_ana, a, ctx_flag + :lhs)
-                b = solve(new_ana, b, ctx_flag + :rhs + :local)
+                a = solve(ana, a, ctx_flag + :lhs + :local)
+                b = solve(ana, b, CtxFlag())
                 Expr(hd, a, b)
             end
-        :($a :: $b) =>
-            begin
-                a = solve(ana, a, ctx_flag + :local)
-                b = solve(ana, b, ctx_flag + :rhs)
-                :($a :: $b)
-            end
-        Expr(hd && (:for || :while), a, b) =>
-            begin
-                ana = child_analyzer!(ana)
-                a   = solve(ana, a, ctx_flag + :lhs)
-                b   = solve(ana, b, ctx_flag + :rhs)
+        Expr(hd && if hd in (:for, :while) end, a, b) =>
+            begin ctx_flag = CtxFlag()
+                ana      = child_analyzer!(ana)
+                a   = solve(ana, a, ctx_flag + :lhs + :local)
+                b   = solve(ana, b, ctx_flag)
                 Expr(hd, a, b)
             end
         Expr(:try, attempt, a, blocks...) =>
-            @quick_lambda Expr(
-                :try,
-                solve(child_analyzer!(ana), attempt, ctx_flag + :rhs),
-                solve(child_analyzer!(ana), a, ctx_flag + :lhs), # catch as
-                map(solve(child_analyzer!(ana), _, ctx_flag + :rhs), blocks)...
-            )
-        Expr(:let, Expr(:block, binds...), a) =>
-            begin
+            @quick_lambda begin ctx_flag = CtxFlag()
+                attempt = solve(child_analyzer!(ana), attempt, ctx_flag)
+                a       = solve(child_analyzer!(ana), a, ctx_flag + :lhs) # catch exc
+                blocks  = map(solve(child_analyzer!(ana), _, ctx_flag), blocks)
+                Expr(:try, attempt, a, blocks...)
+            end
+        Expr(:let, Expr(:block, binds...) || bind && Do(binds = [bind]), a) =>
+            begin ctx_flag = CtxFlag() + :lhs + :local
+                @info :let "???"
                 cur_scope = ana
                 binds = map(binds) do each
                     if !isa(each, LineNumberNode)
                         cur_scope = child_analyzer!(cur_scope)
-                        solve(cur_scope, each, ctx_flag + :lhs)
+                        solve(cur_scope, each, ctx_flag)
                     else
                         each
                     end
                 end
-                a = solve(cur_scope, a, ctx_flag + :rhs)
+                a = solve(cur_scope, a, CtxFlag())
                 Expr(:let, Expr(:block, binds...), a)
             end
         Expr(:do, call, lam) =>
             begin
-                call = solve(ana, call, ctx_flag + :rhs)
-                lam = solve(ana, lam, ctx_flag + :rhs)
+                ctx_flag = CtxFlag()
+                call = solve(ana, call, ctx_flag)
+                lam = solve(ana, lam, ctx_flag)
                 Expr(:do, call, lam)
             end
         Expr(:generator, expr, binds...) =>
-            @quick_lambda begin
+            @quick_lambda begin ctx_flag = CtxFlag()
                 ana = child_analyzer!(ana)
-                binds = map(solve(ana, _, ctx_flag + :lhs), binds)
-                expr = solve(ana, expr, ctx_flag + :rhs)
+                binds = map(solve(ana, _, ctx_flag + :lhs + :local), binds)
+                expr = solve(ana, expr, ctx_flag)
                 Expr(:generator, expr, binds...)
             end
         Expr(:filter, cond, binds...) =>
             @quick_lambda begin
                 binds = map(solve(ana, _, ctx_flag), binds)
-                cond = solve(ana, cond, ctx_flag + :rhs)
+                cond = solve(ana, cond, ctx_flag)
                 Expr(:filter, cond, binds...)
             end
-        Expr(hd && BroadcastFusing(true), a, b) =>
+        Expr(hd && if is_broadcast_fusing(hd) end, a, b) =>
             begin
                 a = solve(ana, a, ctx_flag + :lhs)
-                b = solve(ana, b, ctx_flag + :rhs + :local)
+                b = solve(ana, b, ctx_flag + :rhs + :lexical)
                 Expr(hd, a, b)
             end
         Expr(:local, args...) =>
@@ -170,17 +186,38 @@ function solve(ana, ex, ctx_flag::CtxFlag = CtxFlag())
                 args = map(solve(ana, _, ctx_flag + :global), args)
                 Expr(:local, args...)
             end
+# non-scoped constructs
+        :($a where {$(tps...)}) =>
+            @quick_lambda begin
+                if !ctx_flag.is_lhs && !isempty(tps)
+                    ana = child_analyzer!(ana)
+                    ctx_flag = CtxFlag()
+                    tps = map(solve(ana, _, ctx_flag + :lhs), tps)
+                end
+                a = solve(ana, a, ctx_flag)
+                :($a where {$(tps...)})
+            end
+        :($a :: $b) =>
+            begin
+                a = solve(ana, a, ctx_flag)
+                b = solve(ana, b, CtxFlag())
+                :($a :: $b)
+            end
         Expr(hd, args...) =>
             @quick_lambda begin
+                if is_rhs_sym(hd)
+                    ctx_flag += :rhs
+                end
                 args = map(solve(ana, _, ctx_flag), args)
                 Expr(hd, args...)
-            end
-        sym :: Symbol =>
-            begin
-                error("NOT IMPL") # TODO
             end
         a => a
     end
 end
 
+function solve(ex)
+    ana = top_analyzer()
+    ana, solve(ana, ex)
 end
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
 using JuliaVariables
 using Test
+using NameResolution
 
 @testset "JuliaVariables.jl" begin
+    ana, ex = solve(:(function f(x)
+        let y = x + 1
+            y
+        end
+    end))
+    run_analyzer(ana)
+    println(ana.children[1])
+    println(ex)
     # Write your own tests here.
 end
