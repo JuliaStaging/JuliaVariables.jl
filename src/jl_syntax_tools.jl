a795be93aab5f211bbef903946816ef73edda2fb
thautwarm <twshere@outlook.com>
2019-11-13 23:24:30 +0800

    give up tagless-final approach due to the scope problem of for-loop

---
+++ src/jl_syntax_tools.jl
@@ -0,0 +1,82 @@
+module JuliaSyntaxUtils
+export Unset, FuncArg, FuncHeader, unset, func_arg, func_header, is_func_header
+using MLStyle
+@generated function field_update(main :: T, field::Val{Field}, value) where {T, Field}
+    fields = fieldnames(T)
+    quote
+        $T($([field !== Field ? :(main.$field) : :value for field in fields]...))
+    end
+end
+
+function lens_compile(ex, cache, value)
+    @when :($a.$(b::Symbol).$(c::Symbol) = $d) = ex begin
+        updated =
+            Expr(:let,
+                Expr(:block, :($cache = $cache.$b), :($value = $d)),
+                :($field_update($cache, $(Val(c)), $value)))
+        lens_compile(:($a.$b = $updated), cache, value)
+    @when :($a.$(b::Symbol) = $c) = ex
+        Expr(:let,
+            Expr(:block, :($cache = $a), :($value=$c)),
+            :($field_update($cache, $(Val(b)), $value)))
+    @otherwise
+        error("Malformed update notation $ex, expect the form like 'a.b = c'.")
+    end
+end
+
+function with(ex)
+    cache = gensym("cache")
+    value = gensym("value")
+    lens_compile(ex, cache, value)
+end
+
+macro with(ex)
+    with(ex) |> esc
+end
+
+function with(ex)
+    cache = gensym("cache")
+    value = gensym("value")
+    lens_compile(ex, cache, value)
+end
+
+macro with(ex)
+    with(ex) |> esc
+end
+
+struct Unset end
+unset = Unset()
+
+struct FuncArg
+    name
+    type
+    default
+end
+
+struct FuncHeader
+    name  :: Any
+    args  :: Union{Vector{FuncArg}, Unset}
+    ret   :: Any
+    fresh :: Union{Vector{Any}, Unset}
+end
+
+is_func_header(a::FuncHeader) = a.args != unset
+
+function func_arg(@nospecialize(ex))::FuncArg
+    @match ex begin
+        :($var :: $ty) => @with func_arg(var).type = ty
+        :($var = $default) => @with func_arg(var).default = default
+        var => FuncArg(var, unset, unset)
+    end
+end
+
+function func_header(@nospecialize(ex))::FuncHeader
+    @match ex begin
+        :($hd::$ret) => @with func_header(hd).ret = ret
+        :($f($(args...))) => @with func_header(f).args = map(func_arg, args)
+        :($f where {$(args...)}) => @with func_header(f).fresh = args
+        Expr(:tuple, args...) => FuncHeader(unset, map(func_arg, args), unset, unset)
+        f => FuncHeader(f, unset, unset, unset)
+    end
+end
+end
--- src/scope-interpreter.jl
+++ src/scope-interpreter.jl
@@ -1,19 +1,113 @@
 using Base.Enums
+using NameResolution
+Analyzer = NameResolution.Analyzer
 
+struct Thunk{F}
+    f      :: F
+    result :: Ref{Union{Some, Nothing}}
+end
+
+function force(t::Thunk)
+    res = t.result
+    value = res[]
+    if !isnothing(value)
+        value
+    end
+    value = t.f()
+    res[] = Some(value)
+    value
+end
+
+macro thunk(ex)
+    @match ex begin
+        Expr(:function, head, body) =>
+            Expr(:function, head, quote
+                $Thunk($Ref{$Union{$Some, $Nothing}}(nothing)) do
+                    $body
+                end
+            end)
+        _ => error("malformed thunk!")
+    end
+end
 
 scoper = @structure struct Interpreter
-    Repr = Vector{Symbol}
-    function normal(args::Vararg{Repr})
-        vcat(args...)
-    end
-    jcall = normal
-    function jfor(assigns, block)
-        Meta.isexpr(assigns, :block) && return begin
-            # for i in I, j in J, ...;
+    Repr = Thunk
+    force_cur_ctx = force
+    is_real_ctx = true  # functions, etc.
+    not_real_ctx = false # let-scope, loop-scope, etc.
+    ana_ref = Ref{Analyzer}(top_analyzer())
+
+    is_rhs!(s::Symbol) = require!(ana_ref[], s)
+    is_rhs!(xs::Vector{Symbol}) = foreach(is_rhs!, xs)
+    is_rhs!(a::Repr) = is_rhs!(force_cur_ctx(a))
+
+    is_lhs!(s::Symbol) = begin ana = ana_ref[]
+            enter!(ana, s)
+            require!(ana, s)
         end
-        # for i in I;
+    is_lhs!(xs::Vector{Symbol}) = foreach(is_lhs!, xs)
+    is_lhs!(a::Repr) = is_lhs!(force_cur_ctx(a))
+
+    """
+    make an expression as a scoped expression, with
+    side effects
+    """
+    function with_scope!(ex::Expr, scope::Scope)
+        inner = Expr(ex.head, ex.args...)
+        ex.head = :scope
+        ex.args = Any[scope, inner]
     end
-    function jleaf(e)
+
+    function with_ctx(f::Function, real_ctx::Bool)
+        ana = ana_ref[]
+        try
+            new_ana = child_analyzer(ana, real_ctx)
+            ana_ref[] = new_ana
+            f(), new_ana.solved
+        finally
+            ana_ref[] = ana
+        end
+    end
+
+    @thunk function normal(ex::Any, args::Vararg{Repr})
+        vcat(map(force_cur_ctx, args)...)
+    end
+
+    jcall = normal
+    jtuple = normal
+    jvect = normal
+    jand = normal
+    jor = normal
+
+    @thunk function jfor(ex::Expr, assign, block)
+        assigns = Meta.isexpr(assign, :block) ?
+            # for i in I, j in J, ...;
+            # only I is evaluated in outer ctx
+            assigns = assign.args :
+            [assign]
+        ass_pairs = map(assigns) do ass
+            @match ass begin
+                Expr(:(=), lhs, rhs) => (lhs, rhs)
+                _ => error("invalid julia syntax $assign")
+            end
+        end
+        is_rhs!(ass_pairs[1][2])
+
+        syms, scope = with_ctx(false) do
+            is_lhs!(ass_pairs[1][1])
+            for i = 2:length(ass_pairs)
+                (lhs, rhs) = ass_pairs[i]
+                is_lhs!(lhs)
+                is_rhs!(rhs)
+            end
+            force_cur_ctx(block)
+        end
+        with_scope!(ex, scope)
+        syms
+    end
+    @thunk function jassign(_::Expr, )
+
+    @thunk function jleaf(@nospecialize(e))
         @match e begin
             ::Symbol => Symbol[e]
             _ => Symbol[]
